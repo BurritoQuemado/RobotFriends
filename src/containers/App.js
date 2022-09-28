1448e63837e5c1ad2bbb3bd439742f3c51445bef
Diego Flores <dfmonroy07@gmai.com>
2022-09-28 15:48:59 -0500

    updated to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardsList from "../components/cardsList";
 import SearchBox from "../components/searchBox";
 import Scroll from "../components/Scroll";
 import '../styles/App.css'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => {this.setState({ robots: users})});
+      .then(users => {setRobots(users)});
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-    return (
-        <div className="tc">
-          <h1 className="f1" >RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardsList robots={filteredRobots} />
-          </Scroll>
-        </div>
-    );
-  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  return (
+      <div className="tc">
+        <h1 className="f1" >RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardsList robots={filteredRobots} />
+        </Scroll>
+      </div>
+  );
 }
 
 export default App;
